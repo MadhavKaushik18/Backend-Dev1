d1d07fed98fb548cd596388bb3cac46c9c987b4f
kaushik <madhavkaushik204216@gmail.com>
2026-01-19 21:52:04 +0530

    [Madhav Kaushik]+ commited on nodejs modules

---
+++ 16-01-2025/index.js
@@ -0,0 +1,15 @@
+const fs=require("fs");
+
+
+if(pathname==='/admin'){
+    if(user==='admin' && pass==='1234'){
+        fs.readFile("./admin_dashboard.html",(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.end()
+            }
+        })
+    }
+}
